a0e600123c6e155a9508fbd6d5d79f124e54e9be
MicLord <miclord@yahoo.com>
2026-04-18 05:33:27 +0300

    add project skeleton

---
+++ frontend/src/components/common/index.js
@@ -0,0 +1,11 @@
+import NavItem from "./NavItem/NavItem";
+import SearchBar from "./SearchBar/SearchBar";
+import StatWiget from "./StatWidget/StatWiget";
+import TaskCard from "./TaskCard/TaskCard";
+
+export {
+  NavItem,
+  SearchBar,
+  StatWiget,
+  TaskCard
+}
